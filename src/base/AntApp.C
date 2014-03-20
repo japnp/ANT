253036f64d9bd6a713a310444fa6a6aa46ecf8d4
Japan <japan@ubuntu.(none)>
2014-03-20 10:19:51 -0600

    Beginning new app  Anisotropic Neutron Transport

---
+++ src/base/AntApp.C
@@ -0,0 +1,42 @@
+#include "AntApp.h"
+#include "Moose.h"
+#include "AppFactory.h"
+
+template<>
+InputParameters validParams<AntApp>()
+{
+  InputParameters params = validParams<MooseApp>();
+  return params;
+}
+
+AntApp::AntApp(const std::string & name, InputParameters parameters) :
+    MooseApp(name, parameters)
+{
+  srand(libMesh::processor_id());
+  
+  Moose::registerObjects(_factory);
+  AntApp::registerObjects(_factory);
+
+  Moose::associateSyntax(_syntax, _action_factory);
+  AntApp::associateSyntax(_syntax, _action_factory);
+}
+
+AntApp::~AntApp()
+{
+}
+
+void
+AntApp::registerApps()
+{
+  registerApp(AntApp);
+}
+
+void
+AntApp::registerObjects(Factory & factory)
+{
+}
+
+void
+AntApp::associateSyntax(Syntax & syntax, ActionFactory & action_factory)
+{
+}
